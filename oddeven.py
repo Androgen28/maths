28305799794ed3d9706dfb449d200e8054974dca
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-10-10 09:00:56 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+GNU nano 5.2          oddeve
+print("script by Pritam Behera")
+print
+print("https://github.com/thelearn-tech/maths")
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
